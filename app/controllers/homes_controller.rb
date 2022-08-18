840d6438dae9f3f7f3171923358e555f7c3b6f0d
Kaushik-08 <ckasushicksingh2003@gmail.com>
2022-08-18 11:15:11 +0530

    button changes

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,6 +4,13 @@ class HomesController < ApplicationController
   skip_before_action :ensure_user_logged_in
   def index
     # render 'homes#index'
+    @presence = 0
+    current_user
+    if current_user != nil
+      @presence = 1
+    else 
+      @presence = 0
+    end
   end
 
   def search
